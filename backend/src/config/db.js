2e05a5a4c1ba8f569b9eb9bd70dcbca42b840523
Abhishek <abhishekpagar2020@gmail.com>
2026-06-07 22:06:54 +0530

    Railway database migratoin

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,28 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+pool.getConnection((err, connection) => {
+
+    if (err) {
+
+        console.log("DB Connection Failed");
+        console.log(err.message);
+
+    } else {
+
+        console.log("Railway Database Connected Successfully");
+
+        connection.release();
+
+    }
+
 });
 
 module.exports = pool.promise();
